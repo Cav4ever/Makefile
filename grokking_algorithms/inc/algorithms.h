22cef4ecd70fd26ddf60948ebb6d5e867ae5ead1
Cav4ever <303146950@qq.com>
2018-09-20 20:57:40 +0800

    Offer a multi-level_directory edition Makefile.

--- grokking_algorithms/inc/algorithms.h
+++
@@ -1,6 +0,0 @@
-int binary_search(int[], int, int);
-void selectionSort(int[], int);
-void bubbleSort(int[], int);
-void quickSort(int[], int, int);
-int partition(int[],int, int);
-void swap(int*, int*);
--- grokking_algorithms_75/inc/other.h
+++
@@ -1,2 +0,0 @@
-void showArray(int[], int);
-void swap(int*, int*);
